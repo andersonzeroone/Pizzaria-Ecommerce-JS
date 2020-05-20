7b8539cf595d6f8867a69acfc48d93e9deba5944
P <anderson.pablo02@gmail.ccom>
2020-05-19 23:11:35 -0300

    infoModel

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ pizzaJson.map((item, index) => {
 
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
 
-     pizzaItem.querySelector('.pizza-item--img img').src = item.img
-     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
-     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
-     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     
-     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
 
+        let  key = e.target.closest('.pizza-item').getAttribute('data-key');
+        
+        console.log(pizzaJson[key]);
+        
+        document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
+        document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
+        
         document.querySelector('.pizzaWindowArea').style.opacity = 0;
         document.querySelector('.pizzaWindowArea').style.display = 'flex';
 
@@ -18,5 +28,5 @@ pizzaJson.map((item, index) => {
         }, 200);
      })
 
-    document.querySelector('.pizza-area').appendChild( pizzaItem);    
+    document.querySelector('.pizza-area').appendChild(pizzaItem);    
 })
