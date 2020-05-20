c309e06d03c85994cc3786d986c5ee994807daa1
P <anderson.pablo02@gmail.ccom>
2020-05-19 23:39:02 -0300

    up

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ pizzaJson.map((item, index) => {
 
     pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.',',')}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     
@@ -18,7 +18,11 @@ pizzaJson.map((item, index) => {
         document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
         document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
 
+        document.querySelectorAll('pizzaInfo--size').forEach((size, sizeIndex) =>{
+            size.querySelector('span').innerHTML= pizzaJson[key].sizes[sizeIndex];
+        })
         
         document.querySelector('.pizzaWindowArea').style.opacity = 0;
         document.querySelector('.pizzaWindowArea').style.display = 'flex';
