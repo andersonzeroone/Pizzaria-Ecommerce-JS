d9bdf7cb2c2918c67838ff57cc3565cf5ad95d47
P <anderson.pablo02@gmail.ccom>
2020-05-19 22:51:10 -0300

    up

--- script.js
+++ script.js
@@ -10,8 +10,12 @@ pizzaJson.map((item, index) => {
      pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
 
+        document.querySelector('.pizzaWindowArea').style.opacity = 0;
         document.querySelector('.pizzaWindowArea').style.display = 'flex';
 
+        setTimeout( () =>{
+            document.querySelector('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
      })
 
     document.querySelector('.pizza-area').appendChild( pizzaItem);    
